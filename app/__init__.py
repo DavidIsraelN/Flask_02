9bc85b4df90973300c68f528f3cc2581faeee9f5
David Naki <dlnaki1160@gmail.com>
2025-05-06 16:03:44 +0300

    ex04 from presentation - get categories by year in the query

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,11 @@ from app.routes.prize_routes import prize_bp
 def create_app():
     app = Flask(__name__)
     
-    # Register blueprints for ex01, ex02, ex03 from the presentation
+    # Register blueprints for ex01, ex02, ex03, ex04 from the presentation
     # to get all categories from the Nobel Prize API click on http://localhost:5000/prizes/categories
     # to get all names of the Nobel Prize winners click on http://localhost:5000/prizes/names
     # to get the top category in the Nobel Prize click on http://localhost:5000/prizes/top-category
+    # to get categories by year you can for example to click on http://localhost:5000/prizes/categories-by-year?year=1923 (you can change the year to any year you want)
     app.register_blueprint(prize_bp, url_prefix='/prizes')
     
     return app
